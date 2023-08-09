b814e3bc2382ef01acc3d65af2fb272be6f7bd80
hyeongjin21 <clcjs987@gmail.com>
2023-08-10 08:46:22 +0900

    0810_0846merge

--- back/routes/queries.js
+++ back/routes/queries.js
@@ -1,63 +1,63 @@
 module.exports = {
-    // 일반 회원가입 할때 
-    joinUser: `insert into al_user values( ?,?,?,?,?,now() )`,
+  // 일반 회원가입 할때 
+  joinUser: `insert into al_user values( ?,?,?,?,?,now() )`,
 
-    selectID: `select * from al_user where user_id = ?`,
-    // 아이디 검색
-    searchId: `select * from al_user where user_id = ? and user_pw = ?`,
+  selectID: `select * from al_user where user_id = ?`,
+  // 아이디 검색
+  searchId: `select * from al_user where user_id = ? and user_pw = ?`,
 
-    // 메뉴정보 전체 출력
-    menuInfoAll: `select *from al_menu`,
+  // 메뉴정보 전체 출력
+  menuInfoAll: `select *from al_menu`,
 
-    //메뉴검색 & 카테고리(전체 선택)
-    searchMenu: `select * from al_shop a inner join al_menu b on ( a.shop_seq = b.shop_seq ) where b.menu_name like ?`,
+  //메뉴검색 & 카테고리(전체 선택)
+  searchMenu: `select * from al_shop a inner join al_menu b on ( a.shop_seq = b.shop_seq ) where b.menu_name like ?`,
 
-    //메뉴검색 - 카테고리별 선택
-    searchMenuCategory: `select * from al_shop a inner join al_menu b on ( a.shop_seq = b.shop_seq ) where b.menu_name like ? and b.menu_category = ?`,
+  //메뉴검색 - 카테고리별 선택
+  searchMenuCategory: `select * from al_shop a inner join al_menu b on ( a.shop_seq = b.shop_seq ) where b.menu_name like ? and b.menu_category = ?`,
 
-    //검색 - 메뉴선택 - 지도 위도 경도가져오기
-    shopLocation: `select a.lat, a.lng from al_shop a inner join al_menu b on ( a.shop_seq = b.shop_seq ) where b.shop_seq = ?`,
+  //검색 - 메뉴선택 - 지도 위도 경도가져오기
+  shopLocation: `select a.lat, a.lng from al_shop a inner join al_menu b on ( a.shop_seq = b.shop_seq ) where b.shop_seq = ?`,
 
-    //검색 - 위도 경도값 전체검색
-    selectLocationAll : `select shop_name, lat, lng from al_shop`,
+  //검색 - 위도 경도값 전체검색
+  selectLocationAll: `select shop_name, lat, lng from al_shop`,
 
-    ///// 관리자페이지 /////
-    //사용자 모두 가져오기
-    userAll: `SELECT ROW_NUMBER() OVER (ORDER BY created_at) AS rownum, user_id ,user_pw ,user_name ,user_nick ,user_phone,created_at FROM al_user`,
+  ///// 관리자페이지 /////
+  //사용자 모두 가져오기
+  userAll: `SELECT ROW_NUMBER() OVER (ORDER BY created_at) AS rownum, user_id ,user_pw ,user_name ,user_nick ,user_phone,created_at FROM al_user`,
 
-    // 사용자 검색
-    userNameSearch: `select row_number() over (ORDER BY created_at) AS rownum, user_id ,user_pw ,user_name ,user_nick ,user_phone,created_at from al_user where user_name like ?`,
+  // 사용자 검색
+  userNameSearch: `select row_number() over (ORDER BY created_at) AS rownum, user_id ,user_pw ,user_name ,user_nick ,user_phone,created_at from al_user where user_name like ?`,
 
-    // 사업자 모두 가져오기
-    shopAll: `SELECT ROW_NUMBER() OVER (ORDER BY created_at) AS rownum, shop_seq, shop_name, shop_bno, shop_addr1, shop_addr2, shop_tel, shop_owner, created_at FROM al_shop`,
+  // 사업자 모두 가져오기
+  shopAll: `SELECT ROW_NUMBER() OVER (ORDER BY created_at) AS rownum, shop_seq, shop_name, shop_bno, shop_addr1, shop_addr2, shop_tel, shop_owner, created_at FROM al_shop`,
 
-    // 사업자 검색
-    shopNameSearch: `SELECT ROW_NUMBER() OVER (ORDER BY created_at) AS rownum, shop_name, shop_bno, shop_addr1, shop_addr2, shop_tel, shop_owner, created_at FROM al_shop where shop_name like ?`,
-    
-    
-    // 가게 등록하기
-    insertShop : `INSERT INTO al_shop (shop_name, shop_bno, shop_addr1, shop_addr2, shop_tel, shop_owner) VALUES(?, ?, ?, ?, ?, ?)`,
-    // (shop_name, shop_bno, shop_addr1, shop_addr2, shop_tel, shop_owner) 
-    
-    // 메뉴 등록하기
-    insertMenu:'INSERT INTO al_menu (shop_seq, menu_name, menu_price, menu_desc, menu_type, menu_options, menu_category, menu_ingredient_tag, menu_img) VALUES(?,?,?,?,?,?,?,?,?)',
+  // 사업자 검색
+  shopNameSearch: `SELECT ROW_NUMBER() OVER (ORDER BY created_at) AS rownum, shop_name, shop_bno, shop_addr1, shop_addr2, shop_tel, shop_owner, created_at FROM al_shop where shop_name like ?`,
 
-    // 가게 위치 정보 가져오기
-    shopLocationAll : `SELECT ROW_NUMBER() OVER (ORDER BY created_at) AS rownum, shop_name, shop_addr1, lat, lng FROM al_shop `,
 
-    // 가게 위치 검색
-    shopLocationSearch : `SELECT ROW_NUMBER() OVER (ORDER BY created_at) AS rownum, shop_name, shop_addr1, lat, lng FROM al_shop where shop_name like ?`,
+  // 가게 등록하기
+  insertShop: `INSERT INTO al_shop (shop_name, shop_bno, shop_addr1, shop_addr2, shop_tel, shop_owner) VALUES(?, ?, ?, ?, ?, ?)`,
+  // (shop_name, shop_bno, shop_addr1, shop_addr2, shop_tel, shop_owner) 
 
-    // 리뷰 관리 - 전체 검색
-    reviewAll : `select row_number() over (order by c.created_at) as rownum, 
+  // 메뉴 등록하기
+  insertMenu: 'INSERT INTO al_menu (shop_seq, menu_name, menu_price, menu_desc, menu_type, menu_options, menu_category, menu_ingredient_tag, menu_img) VALUES(?,?,?,?,?,?,?,?,?)',
+
+  // 가게 위치 정보 가져오기
+  shopLocationAll: `SELECT ROW_NUMBER() OVER (ORDER BY created_at) AS rownum, shop_name, shop_addr1, lat, lng FROM al_shop `,
+
+  // 가게 위치 검색
+  shopLocationSearch: `SELECT ROW_NUMBER() OVER (ORDER BY created_at) AS rownum, shop_name, shop_addr1, lat, lng FROM al_shop where shop_name like ?`,
+
+  // 리뷰 관리 - 전체 검색
+  reviewAll: `select row_number() over (order by c.created_at) as rownum, 
 	a.shop_name, b.menu_name, c.user_id, c.created_at, c.review_content, c.review_seq
   from al_shop a inner join al_menu b
     on a.shop_seq = b.shop_seq 
   right outer join al_review c 
     on b.menu_seq  = c.menu_seq `,
 
-    // 리뷰 관리 - 카테고리 검색(글쓴이)
-    reviewSearchUser : `select row_number() over (order by c.created_at) as rownum, 
+  // 리뷰 관리 - 카테고리 검색(글쓴이)
+  reviewSearchUser: `select row_number() over (order by c.created_at) as rownum, 
 	a.shop_name, b.menu_name, c.user_id, c.created_at, c.review_content, c.review_seq
   from al_shop a inner join al_menu b
     on a.shop_seq = b.shop_seq 
@@ -65,8 +65,8 @@ module.exports = {
     on b.menu_seq  = c.menu_seq 
  where c.user_id like ?`,
 
-    // 리뷰 관리 - 카테고리 검색(내용)
-    reviewSearchContent : `select row_number() over (order by c.created_at) as rownum, 
+  // 리뷰 관리 - 카테고리 검색(내용)
+  reviewSearchContent: `select row_number() over (order by c.created_at) as rownum, 
 	a.shop_name, b.menu_name, c.user_id, c.created_at, c.review_content, c.review_seq
   from al_shop a inner join al_menu b
     on a.shop_seq = b.shop_seq 
@@ -74,7 +74,7 @@ module.exports = {
     on b.menu_seq  = c.menu_seq 
  where c.review_content like ?`,
 
-    // 리뷰 관리 - 삭제
-    reviewDelete : `delete from al_review where review_seq = ?`
+  // 리뷰 관리 - 삭제
+  reviewDelete: `delete from al_review where review_seq = ?`
 
 }
